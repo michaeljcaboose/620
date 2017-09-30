2887939017049a1b84909b9368424ddb177ffb40
Michael J Caboose <mj.panebianco@gmail.com>
2017-09-29 22:27:40 -0400

    Milestone 2 complete

---
+++ index.php
@@ -0,0 +1,75 @@
+<html>
+	<head>
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>M.A.S.P.</title>
+		<link href="normalize.css" type="text/css" rel="stylesheet" />
+		<link href="milestone.css" type="text/css" rel="stylesheet" />
+	</head>
+    <body>
+        <div class="navigation">
+	        <?php include("templates/nav.html"); ?>
+        </div>
+        <div class="hero">
+	        <h1>Project Name: Munchuria</h1>
+        </div>
+        
+		<div class="milestone">
+			<div>
+	        	<h2>Team Members, Roles, and Responsibilities</h2>
+	        	<div class="milestone-copy milestone-item">
+	            	<h3>Matthew Panebianco (mp5292@rit.edu)</h3>
+	                <h4>Code Lead</h4>
+	                <p>Create a stable and easy-to-maintain code infrastructure, master of technologies used, and update the project website.</p>
+	                <h4>Research and Design Deputy</h4>
+	                <p>Assist Aishwarya when needed.</p>
+	           	</div>
+	           	<div class="milestone-copy milestone-item">
+	            <h3>Aishwarya Uniyal (axu2523@rit.edu)</h3>
+	                <h4>Research and Design Lead</h4>
+	                    <p>Define the scope of research, plan the interview questions, test prototypes, compare to existing product.</p>
+	                <h4>Documentation Deputy</h4>
+	                    <p>Assist Sovantharith when needed.</p>
+	           	</div>
+	           	<div class="milestone-copy milestone-item">
+		           	<h3>Sovantharith Seng (ss2816@rit.edu)</h3>
+		           	<h4>Team Lead</h4>
+		           	<p>Arrange group meetings, set agenda for group meetings, set deadlines for work, submit team submission if only one submission is needed, and communicate with the professor.</p>
+		           	<h4>Documentation Lead</h4>
+		           	<p>Maintain documentations for the projects and team meetings.
+	                </p>
+	                <h4>Code Deputy</h4>
+	                <p>Assist Matthew when needed.</p>
+	           	</div>
+	           	
+	            <div class="milestone-copy milestone-item">
+		            <h3>Peter Yeung (pxy9548@rit.edu)</h3>
+	                <h4>Communication Lead</h4>
+	                    <p>Recruit users for interviews, interact with the participants, collect and analyze qualitative data.</p>
+	                <h4>Team Deputy</h4>
+	                    <p>Assist Sovantharith when needed.</p>
+	            </div>
+			</div>
+	        <div>    
+	        	<h2>Softwares</h2>
+	        	<div class="milestone-copy milestone-item"> 
+		            <ul>
+			            <li>Google Suite for storage and documentation</li>
+			            <li>Google Hangouts for team communication and online meeting</li>
+			            <li>GitHub for code repo</li>
+			            <li>Prototyping software TBD</li>
+		            </ul>
+	        	</div>
+	        </div>
+	        <div >    
+	        	<h2>Grounds of Expulsion</h2>
+	        	<div  class="milestone-copy milestone-item">
+		        	<ul>
+		            	<li>Misses 2 meetings without providing an upfront and reasonable explanation.</li>
+						<li>Does not provide any contributions to projects.</li>
+						<li>Plagiarize someone else's work.</li>
+		        	</ul>
+	        	</div>
+	        </div>
+		</div>
+    </body>
+</html>
---
+++ milestone2.php
@@ -0,0 +1,180 @@
+<html>
+	<head>
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>Milestone 2</title>
+		<link href="normalize.css" type="text/css" rel="stylesheet" />
+		<link href="milestone.css" type="text/css" rel="stylesheet" />
+	</head>
+	<body>
+		<div class="navigation">
+	        <?php include("templates/nav.html"); ?>
+		</div>
+        <div class="milestone">
+            <h2>Milestone 2 - Contextual Inquiry</h2>
+            <img src="media/images/milestone2/ad1-min.png" alt="Working on Contextual Inquiry" />
+            <div class="milestone-copy">
+                <div class="milestone-item">
+                    <h3>Schedule</h3>
+                    <ul>
+                        <li>09/19: Group meeting at the Wallace Library to revise Milestone 1</li>
+                        <li>09/22: Group meeting at the Wallace Library to finish revising Milestone 1. Start discussing
+                            Milestone 2. Set deadline for interview question creation by 09/24 at 10PM.</li>
+                        <li>09/24: Online group meeting to finalize the interview questions. Between the four of us, we
+                            created about 30 different questions. Through discussion, we filtered the questions down to
+                            only 15. The interviews were planned to start on 09/25 and must be finished by 09/27.</li>
+                        <li>09/25: Team members started the interview process. Each member was tasked to do three
+                            interviews. The interviews were recorded since they were conducted by individual team member.
+                            Team members were expected to create notes of the interviews for the meeting on 09/28. Brief
+                            summaries of all the team member’s interviews are listed in the following section.</li>
+                        <li>09/28: Interpretation session at Booth Building. Roughly 200 post-it notes were made from
+                            all the notes of the interviews. We first intended to color-code the notes; however, we
+                            quickly ran out of one color. Hence, the mixed color post-it notes that you will see in the
+                            pictures. We first put up all the post-it notes in an unorganized manner on two whiteboards.
+                            Then we followed the provided instructions and started to cluster and re-organize the notes
+                            into different topics. The session took about 3 hours. Pictures of the progress throughout
+                            the session are included below.</li>
+                        <li>09/29: Interpretation session continued at Booth Building. We continued the process and
+                            started to reorganize, label, and relabel different topics. After 4 hours of discussion,
+                            we finalized the affinity diagram. Pictures of the progress throughout the sessions are
+                            included below.</li>
+                    </ul>
+                </div>
+
+                <div class="milestone-item">
+                    <h3>Brief Summaries of the Interviews</h3>
+                    <div>
+                        <h4>Matthew</h4>
+                        <p>I conducted my interviews in informal and private settings using the Voice Memo app on my
+                            phone. I wanted to be able to focus on driving the interview rather than having to split my
+                            attention between taking notes and asking questions. This allowed for a better back-and-forth
+                            dynamic where I could expand upon the responses I was getting from my interview subjects.
+                            The interviews lasted between 10 to 20 minutes. I'd wait until hours after the interview to
+                            go back and listen. I would then summarize the details of the interview into finer points for
+                            our interpretation session. </p>
+                    </div>
+                    <div>
+                        <h4>Aishwarya</h4>
+                        <p>Conducted three interviews of graduate students. The interviews were documented through an
+                            audio recorder and span between 17 to 45 minutes. There were instances when the conversation
+                            felt to be drifting away from the topic of focus but constant and improvised refinement of
+                            certain questions specific to the user provided really useful perspectives. I prefered not
+                            to take notes during the interview and saved it for later when I heard the recordings again.
+                            The three users provided a varying degree of perspectives - while one preferred to order
+                            together, the other was fine with people ordering their food separately in a social setting.
+                            They unanimously agreed on the fact that ordering and deciding upon a place to eat takes the
+                            most time and gets even more confusing when views vary and there is a constraint on ordering
+                            from a particular place.
+                        </p>
+                    </div>
+                    <div>
+                        <h4>Sovantharith</h4>
+                        <p>I conducted my interviews in person at the Center for Cybersecurity, where my office is
+                            located. My participants were my labmates. Two of them were males and one was female. The
+                            conversations were all recorded on the Voice Memo app on my smartphone. The interview sessions
+                            lasted about 10 minutes on average. The interviews were conducted on 09/25 and 09/26. I
+                            produced the notes from the interview on 09/27 for the interpretation session.</p>
+                        <p>The interview
+                            sessions were quite short because the interviewees didn’t have a lot of experience or thought
+                            that they want to share. They didn’t think that they have a lot of interesting details to share.
+                            I was unsuccessful in guiding them to speak more about their experience without leading them
+                            to the answers that I want. However, there is a consensus that in a group setting, it is hard
+                            to decide where to eat or what to order. They also all agree that wait time at the restaurant
+                            is the main irritant about going out to eat. Last but not least, one interesting observation
+                            is that the interviewees had different views about pictures on a menu; one person thought that
+                            it is not classy to have pictures on a menu; one person thought that the pictures should always
+                            be included; the last interviewee was indifferent.</p>
+                    </div>
+                    <div>
+                        <h4>Peter</h4>
+                        <p>I conducted my interviews in open space setting and online.  The first participant was Deaf
+                            and I asked him for a permission to have a video recorded.  I clarified the questions to him
+                            using visual American Sign Language.  That interview session lasted about 20 minutes.  I
+                            translated from ASL to written English into the notes after the interview ended.  For the
+                            online interviews, the other two participants were extremely busy and had conflict with
+                            schedule. Two participants had a lot of experience eating out and eating inside the restaurant.
+                            I shared them interview questions through the Google Document.  They were typing their
+                            responses while I followed them up with the questions to explain more about their experience.
+                        </p>
+                    </div>
+                </div>
+
+                <div class="milestone-item">
+                    <h3>Interpretation Session</h3>
+                    <p>Some problems that we did notice during the interviews,</p>
+                    <ul>
+                        <li>
+                            From the subjects of Peter, the requirements at least have some features which let the customers
+                            communicate that they are Deaf.
+                        </li>
+                        <ul>
+                            <li>
+                                Deaf people prefer the iPad or smartphones to place their orders to be sent to the system
+                                instead of showing and/or pointing the entree from the menu.
+                                It also applies to the people with disabilities.
+                            </li>
+                        </ul>
+                        <li>Phone ordering tends to be most hectic as most places are busy around the time (lunch/dinner)
+                            - noise, operator temperament, accent etc. also play a role in effective communication.
+                            Wrong orders are a common result of this.
+                        </li>
+                        <li>Unknown ingredients could be better represented or presented in the simplest visual-textual manner.</li>
+                        <li>Interpersonal dynamics influence decision making and also vary with the formality of the situation.
+                        </li>
+                        <li>Most time gets consumed in decision making regardless of whether the group is eating out or
+                            ordering in. This further increases when people are eating out, because after deciding on
+                            the place they must decide on the menu.
+                        </li>
+                        <li>
+                            While using apps when ordering, time gets wasted when looking up details on food or for coupons.
+                        </li>
+                        <li>People dislike the fact that on certain apps, the must jump from the medium of a phone
+                            screen to a call - it is disruptive to the flow.
+                        </li>
+                        <li>Allergies, foreign ingredients and other dietary restrictions need to be made clear through
+                            check boxes or comments - some places do not cater to these preferences and this info is
+                            made available at a later stage - on phone call etc.
+                        </li>
+                        <li>Closing times also affect these decisions, especially when orders are placed during parties
+                            which are usually at night. Quick alternatives need to be figured out or plans need to be
+                            dropped because the place of choice closes down at a particular hour.
+                        </li>
+                    </ul>
+                </div>
+                <div class="milestone-item">
+                    <h3>Affinity Diagram</h3>
+                    <p>PICTURES! Worth a thousand words!</p>
+                    <figure>
+                        <img src="media/images/milestone2/ad1-min.png" alt="Beginning the Contextual Inquiry"/>
+                        <figcaption>Beginning the Contextual Inquiry. <a href="media/images/milestone2/ad1-min-fullres.png">Click here for the full-res photo</a></figcaption>
+                    </figure>
+                    <figure>
+                        <img src="media/images/milestone2/ad2-min.png" alt="Post-it notes"/>
+                        <figcaption>Some data points. <a href="media/images/milestone2/ad2-min-fullres.png">Click here for the full-res photo</a></figcaption>
+                    </figure>
+                    <figure>
+                        <img src="media/images/milestone2/ad3-min.png" alt="Reorganization of the Post-its begins. "/>
+                        <figcaption>Re-organization of the Post-its begins. <a href="media/images/milestone2/ad3-min-fullres.png">Click here for the full-res photo</a></figcaption>
+                    </figure>
+                    <figure>
+                        <img src="media/images/milestone2/ad4-min.png" alt="Lots and lots of post-its"/>
+                        <figcaption>All of our original post-it notes. <a href="media/images/milestone2/ad4-min-fullres.png">Click here for the full-res photo</a></figcaption>
+                    </figure>
+                    <figure>
+                        <img src="media/images/milestone2/ad5-min.png" alt="Categorizing Post-it notes"/>
+                        <figcaption>Almost completed categorization of the Post-It notes. <a href="media/images/milestone2/ad5-min-fullres.png">Click here for the full-res photo</a></figcaption>
+                    </figure>
+                    <figure>
+                        <img src="media/images/milestone2/ad6-min.png" alt="Post-it notes laid out on table."/>
+                        <figcaption>Second day of contextual inquiry begins. <a href="media/images/milestone2/ad6-min-fullres.png">Click here for the full-res photo</a></figcaption>
+                    </figure>
+                    <figure>
+                        <img src="media/images/milestone2/ad7-min.png" alt="Final Affinity Diagram"/>
+                        <figcaption>Our old-fashioned Affinity Diagram. <a href="media/images/milestone2/ad7-min-fullres.png">Click here for the full-res photo</a></figcaption>
+                    </figure>
+
+                </div>
+
+            </div>
+        </div>
+	</body>
+</html>
