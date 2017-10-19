b10dea7a0d84b189fcb7ed6c4649081e10d72b6b
Matthew Panebianco <mpane1@u.brockport.edu>
2017-10-18 23:40:39 -0400

    v2 of milestone3

--- milestone3.php
+++ milestone3.php
@@ -80,32 +80,233 @@
                             We rewrote the behavioral pattern scales for a cleaner starting point. Then, we proceeded to
                             identify significant behavior patterns (STEP 4).The followings are the clusters that we found.
                         </p>
-                    <p>We then synthesize the characteristics and define their goals. (STEP 5)</p>
                         <ul>
-                            <li>Matthew’s subjects: M1 and M2</li>
-                            <li>Aish’s subjects: A1, A2, and A3</li>
-                            <li>Sovantharith’s subjects: S1, S2, and S3</li>
-                            <li>Peter’s subjects: P1, P2, and P3</li>
+                            <li><strong>S1 and P1</strong>: Do not go out often, are okay with remembering multiple orders,
+                                prefer to order online and having a fair discussion, prefer to eat/order alone, and they
+                                are comfortable with technology.
+                            </li>
+                            <li><strong>S2, P2, and S3</strong>: Go out often, are good with remembering multiple orders,
+                                like to make decisions that are quick, and prefer to eat/order as a group.</li>
+                            <li><strong>A1, A3, and P3</strong>: Do not go out often. Are okay with decision making, make
+                                decision that is a compromise between quick and fair, and are comfortable with technology.
+                            </li>
                         </ul>
-                        <p>We then came up with the roles and grouped the subjects to those roles. (STEP 1)</p>
+
+                        <p>We then synthesize the characteristics and define their goals. (STEP 5)</p>
                         <ul>
-                            <li>Role 1: Person who initiate the voting process for a group. (S2, P2, A2)</li>
-                            <li>Role 2: Person who participate in the voting process for a group. (S1, S3, P1, P3, A2, A3, A1)</li>
-                            <li>Role 3: Person who prefer to order/eat out alone (A1)</li>
+                            <li><strong>Persona 1</strong></li>
+                            <ul>
+                                <li>Demographics</li>
+                                <ul>
+                                    <li>RIT graduate student</li>
+                                    <li>Male</li>
+                                    <li>25</li>
+                                    <li>Single</li>
+                                </ul>
+                                <li>Behaviors</li>
+                                <ul>
+                                    <li>Doesn’t go out often</li>
+                                    <li>Prefers ordering online</li>
+                                    <li>Is OK with remembering</li>
+                                    <li>More towards fair decision</li>
+                                    <li>Preference for self</li>
+                                    <li>Comfortable with technology</li>
+
+                                </ul>
+                                <li>End Goals</li>
+                                <ul>
+                                    <li>Order accuracy</li>
+                                    <li>Doesn’t wish to expend money or effort in going out</li>
+                                    <li>Find the best place to eat</li>
+
+                                </ul>
+                                <li>Experience Goals</li>
+                                <ul>
+                                    <li>Minimized personal interaction</li>
+                                    <li>Order verification</li>
+                                    <li>Explore before deciding</li>
+                                    <li>Able to change vote</li>
+                                    <li>Ability to do it online</li>
+                                    <li>Can use the service independently</li>
+                                    <li>Can access the service on multiple platforms</li>
+
+                                </ul>
+                                <li>Life Goals</li>
+                                <ul>
+                                    <li>Comfort is of utmost importance</li>
+                                    <li>Wishes to be come across as smart and disciplined to others</li>
+                                </ul>
+                                <li>Frustrations / Pain Points</li>
+                                <ul>
+                                    <li>Does not want to interact with strangers.</li>
+                                    <li>Has overpaid in social situations way too many times.</li>
+                                    <li>People cannot make up their mind about their orders.</li>
+                                </ul>
+                            </ul>
+
+                            <li><strong>Persona 2</strong></li>
+                            <ul>
+                                <li>Demographics</li>
+                                <ul>
+                                    <li>UofR graduate student</li>
+                                    <li>Female</li>
+                                    <li>26</li>
+                                    <li>Married</li>
+                                </ul>
+                                <li>Behaviors</li>
+                                <ul>
+                                    <li>Goes out often</li>
+                                    <li>Remembers orders easily</li>
+                                    <li>Likes making decisions</li>
+                                    <li>Goes with the group’s choices</li>
+                                    <li>Prefers quick decisions over fair decisions</li>
+                                </ul>
+                                <li>End Goals</li>
+                                <ul>
+                                    <li>Experiments with new styles, hence utmost priority on accuracy of orders</li>
+                                    <li>Quick delivery</li>
+                                    <li>Doesn’t want any opinions to clash</li>
+                                </ul>
+                                <li>Experience Goals</li>
+                                <ul>
+                                    <li>Options in apps to save preferences for self and others</li>
+                                    <li>Being the champion of the group dining decisions</li>
+                                    <li>Wants to be clearly understood and also understand in a crowd</li>
+                                    <li>Able to make quick decisions</li>
+                                </ul>
+                                <li>Life Goals</li>
+                                <ul>
+                                    <li>Likes to take things easy, focuses on self-betterment</li>
+                                    <li>Spend quality time with family and friends</li>
+                                </ul>
+                                <li>Frustrations / Pain Points</li>
+                                <ul>
+                                    <li>Arguments over food or place choices</li>
+                                    <li>People stuck on eating the same things</li>
+                                    <li>Super democratic choices like pizza; no variety in orders</li>
+                                    <li>Steps that consume/waste time</li>
+                                </ul>
+                            </ul>
+
+                            <li><strong>Persona 3</strong></li>
+                            <ul>
+                                <li>Demographics</li>
+                                <ul>
+                                    <li>RIT undergraduate student</li>
+                                    <li>19</li>
+                                    <li>Male</li>
+                                    <li>Single</li>
+                                </ul>
+                                <li>Behaviors</li>
+                                <ul>
+                                    <li>Not too outgoing</li>
+                                    <li>OK with making decisions</li>
+                                    <li>In-between quick and fair decisions</li>
+                                    <li>Comfortable with tech</li>
+                                </ul>
+                                <li>End Goals</li>
+                                <ul>
+                                    <li>Food is relatively close to specific order</li>
+                                    <li>Prefers eating alone</li>
+                                    <li>Doesn't wish to cook - Doesn’t care much about speed of service or cost fairness</li>
+                                </ul>
+                                <li>Experience Goals</li>
+                                <ul>
+                                    <li>“Go with the flow” attitude</li>
+                                    <li>Food is an excuse to socialize</li>
+                                    <li>Ordering for others should be less strenuous</li>
+                                    <li>Doesn’t really worry if the order is messed up - finds such problems to only
+                                        generate more ruckus, ie. wait for another order or get a coupon to order from a
+                                        place that messed up the order once again</li>
+                                    <li>Looks for vegan foods</li>
+                                </ul>
+                                <li>Life Goals</li>
+                                <ul>
+                                    <li>Wishes for more confidence in social settings</li>
+                                </ul>
+                                <li>Frustrations / Pain Points</li>
+                                <ul>
+                                    <li>Group choices do not account for vegan places</li>
+                                    <li>People who take a lot of time deciding on what to order</li>
+                                    <li>Not many vegan options or methods of finding vegan restaurants</li>
+
+                                </ul>
+                            </ul>
+
+                            <li><strong>Persona 4</strong></li>
+                            <ul>
+                                <li>Demographics</li>
+                                <ul>
+                                    <li>Monroe Community College student</li>
+                                    <li>25</li>
+                                    <li>Female</li>
+                                    <li>Single</li>
+                                </ul>
+                                <li>Behaviors</li>
+                                <ul>
+                                    <li>Not frequently - Avoids parties- Prefers In-person dining rather than ordering</li>
+                                    <li>Comfortable with tech</li>
+                                    <li>Precise requirements; Avoids parties - In social scenarios, orders separately for herself</li>
+                                    <li>Prefers to order on the phone or pick-up in person</li>
+                                    <li>Picks up deliveries whenever possible</li>
+                                    <li>Won't spend on parties</li>
+                                </ul>
+                                <li>End Goals</li>
+                                <ul>
+                                    <li>Exact orders</li>
+                                    <li>Highly consistent quality</li>
+                                    <li>Quick delivery</li>
+                                </ul>
+                                <li>Experience Goals</li>
+                                <ul>
+                                    <li>Accurate orders</li>
+                                    <li>Best deals</li>
+                                </ul>
+                                <li>Life Goals</li>
+                                <ul>
+                                    <li>Get a job</li>
+                                    <li>Make it to the Dean's List</li>
+                                </ul>
+                                <li>Frustrations / Pain Points</li>
+                                <ul>
+                                    <li>People wasting time on orders or socializing</li>
+                                    <li>Restaurant take-out waiting lines</li>
+                                </ul>
+                            </ul>
                         </ul>
-                        <p>We then identify the behavioral variables (STEP 2). We decided to pair the variables into
-                            sliding scales
+
+
+                    </div>
+
+                    <div>
+                        <h4>October 18th, 2017</h4>
+                        <p>
+                            We checked for completeness and redundancy. (STEP 6)
                         </p>
+                        <p>We then designate the persona to the corresponding types as the following: (STEP 7)</p>
                         <ul>
-                            <li>Frequency of ordering out</li>
-                            <li>How well can they handle orders?</li>
-                            <li>How do they feel about making decisions?</li>
-                            <li>Phone ordering vs online ordering</li>
-                            <li>Quick decisions vs Fair decisions</li>
-                            <li>Order for themselves vs Ordering as a group</li>
-                            <li>Tech savvy</li>
+                            <li>Persona 1:</li>
+                            <li>Persona 2: </li>
+                            <li>Persona 3:</li>
+                            <li>Persona4: Anti-Persona</li>
                         </ul>
-                        <p>We then mapped the interview subjects to the behavioral variables. (STEP 3)</p>
+                        <p>We then expanded the description of attributes and behaviors of the personas (STEP 8).</p>
+                        <ul>
+                            <li>
+                                We generated names using <a herf="http://random-name-generator.info">http://random-name-generator.info</a>
+                                with the setting of 10 average male and female names. Then we selected the first 4 of the list.
+                            </li>
+                            <li>
+                                Fictional situations such as jobs, pet peeves, and lifestyles were added with realistic
+                                constraints of residents in the area.
+                            </li>
+                            <li>Persona photos were obtained from <a href="www.pexels.com">www.pexels.com</a>,
+                                <a href="http://www.gettyimages.com/creative-images/royaltyfree">
+                                    http://www.gettyimages.com/creative-images/royaltyfree
+                                </a>
+                            </li>
+                        </ul>
+
                     </div>
                 </div>
 
