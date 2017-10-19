425889ea3daed6b703a19085b7bbddb2b09e7233
Michael J Caboose <mj.panebianco@gmail.com>
2017-10-18 22:57:34 -0400

    initial creation of milestone3

---
+++ milestone3.php
@@ -0,0 +1,123 @@
+<html>
+	<head>
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>Milestone 3</title>
+		<link href="normalize.css" type="text/css" rel="stylesheet" />
+		<link href="milestone.css" type="text/css" rel="stylesheet" />
+	</head>
+	<body>
+		<div class="navigation">
+	        <?php include("templates/nav.html"); ?>
+		</div>
+        <div class="hero">
+            <h1>Milestone 3 - Personas</h1>
+        </div>
+        <div class="milestone">
+            <h2>Generate Personas from the Contextual Inquiries in Milestone 2</h2>
+            <img src="media/images/milestone2/ad1-min.png" alt="Working on Contextual Inquiry" />
+            <div class="milestone-copy">
+
+                <div class="milestone-item">
+                    <h3>Personas</h3>
+                    <div>
+                        <h4>Primary Persona</h4>
+                        <p></p>
+                    </div>
+                    <div>
+                        <h4>Secondary Persona #1</h4>
+                        <p></p>
+                    </div>
+                    <div>
+                        <h4>Secondary Persona #2</h4>
+                        <p></p>
+                    </div>
+                    <div>
+                        <h4>Anti-Persona</h4>
+                        <p></p>
+                    </div>
+                </div>
+
+                <div class="milestone-item">
+                    <h3>Schedule</h3>
+                    <div>
+                        <h4>October 16th, 2017</h4>
+
+                            <p>First meeting to work on Milestone 3. The meeting took place at the Booth Building from 7:30PM
+                                to 9PM. All team members were present.
+                            </p>
+                            <p>First, we summarized all the interviews that we did and gave the subjects code names.</p>
+                            <ul>
+                                <li>Matthew’s subjects: M1 and M2</li>
+                                <li>Aish’s subjects: A1, A2, and A3</li>
+                                <li>Sovantharith’s subjects: S1, S2, and S3</li>
+                                <li>Peter’s subjects: P1, P2, and P3</li>
+                            </ul>
+                            <p>We then came up with the roles and grouped the subjects to those roles. (STEP 1)</p>
+                            <ul>
+                                <li>Role 1: Person who initiate the voting process for a group. (S2, P2, A2)</li>
+                                <li>Role 2: Person who participate in the voting process for a group. (S1, S3, P1, P3, A2, A3, A1)</li>
+                                <li>Role 3: Person who prefer to order/eat out alone (A1)</li>
+                            </ul>
+                            <p>We then identify the behavioral variables (STEP 2). We decided to pair the variables into
+                                sliding scales
+                            </p>
+                            <ul>
+                                <li>Frequency of ordering out</li>
+                                <li>How well can they handle orders?</li>
+                                <li>How do they feel about making decisions?</li>
+                                <li>Phone ordering vs online ordering</li>
+                                <li>Quick decisions vs Fair decisions</li>
+                                <li>Order for themselves vs Ordering as a group</li>
+                                <li>Tech savvy</li>
+                            </ul>
+                        <p>We then mapped the interview subjects to the behavioral variables. (STEP 3)</p>
+
+                    </div>
+
+                    <div>
+                        <h4>October 17th, 2017</h4>
+                        <p>
+                            We rewrote the behavioral pattern scales for a cleaner starting point. Then, we proceeded to
+                            identify significant behavior patterns (STEP 4).The followings are the clusters that we found.
+                        </p>
+                    <p>We then synthesize the characteristics and define their goals. (STEP 5)</p>
+                        <ul>
+                            <li>Matthew’s subjects: M1 and M2</li>
+                            <li>Aish’s subjects: A1, A2, and A3</li>
+                            <li>Sovantharith’s subjects: S1, S2, and S3</li>
+                            <li>Peter’s subjects: P1, P2, and P3</li>
+                        </ul>
+                        <p>We then came up with the roles and grouped the subjects to those roles. (STEP 1)</p>
+                        <ul>
+                            <li>Role 1: Person who initiate the voting process for a group. (S2, P2, A2)</li>
+                            <li>Role 2: Person who participate in the voting process for a group. (S1, S3, P1, P3, A2, A3, A1)</li>
+                            <li>Role 3: Person who prefer to order/eat out alone (A1)</li>
+                        </ul>
+                        <p>We then identify the behavioral variables (STEP 2). We decided to pair the variables into
+                            sliding scales
+                        </p>
+                        <ul>
+                            <li>Frequency of ordering out</li>
+                            <li>How well can they handle orders?</li>
+                            <li>How do they feel about making decisions?</li>
+                            <li>Phone ordering vs online ordering</li>
+                            <li>Quick decisions vs Fair decisions</li>
+                            <li>Order for themselves vs Ordering as a group</li>
+                            <li>Tech savvy</li>
+                        </ul>
+                        <p>We then mapped the interview subjects to the behavioral variables. (STEP 3)</p>
+                    </div>
+                </div>
+
+                <div class="milestone-item">
+                    <h3>Lessons Learned</h3>
+                    <p>We feel as though we did not collect enough information from our interview subjects. This results
+                        in limited data for us to work with during this process.
+                    </p>
+                    <p>We did not ask them enough specific questions about their experiences and goals.</p>
+                </div>
+
+            </div>
+        </div>
+	</body>
+</html>
